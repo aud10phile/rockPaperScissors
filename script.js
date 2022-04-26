e06829175eaf91d325dba7b863a34aff903fda4f
Jeston Lewis <jeston.lewis@protonmail.com>
2022-04-25 20:44:44 -0500

    Started mapping out pseudo code for creation

---
+++ script.js
@@ -0,0 +1,6 @@
+// Create players
+// Define superiority
+// Start round 1 - check winner, display winner, prompt players to pick a move, display moves
+// Start round 2 - check winner, display winner, prompt players to pick a move, display moves
+// Start round 3 - check winner (if one player has two wins, end game), display winner, prompt players to pick a move, display moves
+// End game - 
