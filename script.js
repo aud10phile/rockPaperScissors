06894ae980a8325d29d65df86b6336cb07b96902
Jeston Lewis <jeston.lewis@protonmail.com>
2022-04-27 13:07:05 -0500

    started building out code.

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 // Create players
 // Define superiority
 // Start round 1 - check winner, display winner, prompt players to pick a move, display moves
+// If no winner, repeat round
 // Start round 2 - check winner, display winner, prompt players to pick a move, display moves
+// If no winner, repeat round
 // Start round 3 - check winner (if one player has two wins, end game), display winner, prompt players to pick a move, display moves
+// If no winner, repeat round
 // End game - display winning message
+
+let playerChoice =  prompt('Choose Rock, Paper or Scissors', '')
+
+function checkWinner() {
+    const 
+}
+
+function computerPlay(){
+
+}
+
+const playerSelection = '';
+const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection))
+
+// for (rock, paper, scissors);
