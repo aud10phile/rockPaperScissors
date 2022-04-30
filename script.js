af696cb68b7d5862ec9dfae89682d9a01a6bf5bf
Jeston Lewis <jeston.lewis@protonmail.com>
2022-04-30 10:35:02 -0400

    Added player choice system

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-// Create players
-// Define superiority
-// Start round 1 - check winner, display winner, prompt players to pick a move, display moves
-// If no winner, repeat round
-// Start round 2 - check winner, display winner, prompt players to pick a move, display moves
-// If no winner, repeat round
-// Start round 3 - check winner (if one player has two wins, end game), display winner, prompt players to pick a move, display moves
-// If no winner, repeat round
-// End game - display winning message
+/* Create players
+Define superiority
+Start round 1 - check winner, display winner, prompt players to pick a move, display moves
+If no winner, repeat round
+Start round 2 - check winner, display winner, prompt players to pick a move, display moves
+If no winner, repeat round
+Start round 3 - check winner (if one player has two wins, end game), display winner, prompt players to pick a move, display moves
+If no winner, repeat round
+End game - display winning message */
 
 let playerChoice =  prompt('Choose Rock, Paper or Scissors', '')
+playerChoice = playerChoice.toLowerCase('')
 
-function checkWinner() {
-    const 
-}
-
-function computerPlay(){
-
-}
-
-const playerSelection = '';
-const computerSelection = computerPlay();
-
-console.log(playRound(playerSelection, computerSelection))
-
-// for (rock, paper, scissors);
+if (playerChoice === 'rock') {
+    console.log('You chose Rock');
+} else if (playerChoice === 'paper') {
+    console.log('You chose Paper'); 
+} else if (playerChoice === 'scissors') {
+    console.log('You chose Scissors');
+} else {
+    prompt('Please choose Rock, Paper or Scissors', '')
+};
