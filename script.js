11716460d371ff26e095443c5f28b2b9cdbb5de7
Jeston Lewis <jeston.lewis@protonmail.com>
2022-05-01 10:54:59 -0400

    Got player selection to loop if rock paper or scissors is not the input

--- script.js
+++ script.js
@@ -6,54 +6,57 @@ function playerChoice() {
     let playerSelection =  prompt('Choose Rock, Paper or Scissors', '')
     playerSelection = playerSelection.toLowerCase('');
 
-    if (playerSelection === 'rock') {
+    if (playerSelection === choices[0]) {
         console.log('You played Rock');
-        return 'rock'
-    } else if (playerSelection === 'paper') {
+        return choices[0]
+    } else if (playerSelection === choices[1]) {
         console.log('You played Paper'); 
-        return 'paper'
-    } else {
+        return choices[1]
+    } else if (playerSelection === choices[2]) {
         console.log('You played Scissors');
-        return 'scissors'
+        return choices[2]
+    } else {
+        playerChoice()
     }};
 
 function computerChoice() {
     let computerSelection = Math.floor(Math.random()*choices.length)
     if (computerSelection === 0) {
         console.log('Computer played Rock');
-        return 'rock'
+        return choices[0]
     } else if (computerSelection === 1) {
         console.log('Computer played Paper');
-        return 'paper'
+        return choices[1]
     } else {
         console.log('Computer played Scissors');
-        return 'scissors'
+        return choices[2]
     };
 }
 
 // Define superiority
 function superiority() {
-    'rock' > 'scissors'
-    'scissors' > 'paper'
-    'paper' > 'rock'
+    // rock beats scissors
+    choices[0] > choices[2];
+    // scissors beats paper
+    choices[2] > choices[1];
+    // paper beats rock
+    choices[1] > choices[0];
 }
 
 // Start round - check winner, display winner, prompt players to pick a move, display moves. If no winner, repeat round
 function round(playerChoice, computerChoice) {
     // check for winner
     if (computerChoice > playerChoice) {
-        console.log('Computer wins')
-        return 'Computer wins';
+        console.log('Computer wins');
     } else if (playerChoice > computerChoice) {
-        console.log('Player wins')
-        return 'Player wins';
+        console.log('Player wins');
     } else {
-        console.log('It\'s a tie!')
-        return 'It\'s a tie!';
+        console.log('It\'s a tie!');
     }};
 
 function game() {
     for (let i = 0; i < 5; i++) {
+        console.log('Round ' + (i+1))
         round();
         playerChoice();
         computerChoice();
